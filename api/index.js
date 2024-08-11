a8bba584277526904ce6d8877eeac5add61524da
Zach Morris <sbpayetc@gmail.com>
2024-08-11 17:10:42 -0400

    Hash password with bcryptjs
    Obscure the password variable with cryptographic hashing so future users
    have account security even within the database

--- api/index.js
+++ api/index.js
@@ -6,10 +6,13 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const cookieParser = require("cookie-parser")
+const bcrypt = require("bcryptjs");    //Password hashing
+
 
 
 mongoose.connect(process.env.MONGODB_URL);
 const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const User = require("./models/User");
 
@@ -36,10 +39,29 @@ app.get("/profile", (req, res) => {
     }
 
 })
-app.post("/register", async (req, res, ) => {
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+    const foundUser = await User.findOne({username});
+    if (foundUser) {
+        const passOk = bcrypt.compareSync(password, foundUser.password);
+        if (passOk) {
+            jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {    //Payload  Async token
+                if (err) throw err;
+                res.cookie("token", token, {sameSite:"none", secure:true}).json({    //Name of token and Value of token
+                    id: foundUser._id,
+                })
+            });
+        }
+    }
+})
+app.post("/register", async (req, res) => {
     const {username, password} = req.body;
     try {
-        const createdUser = await User.create({username, password});    //Async json
+        const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
+        const createdUser = await User.create({    //Async json
+            username:username, 
+            password:hashedPassword,
+        });
         jwt.sign({userId:createdUser._id,username}, jwtSecret, {}, (err, token) => {    //Payload  Async token
             if (err) throw err;
             res.cookie("token", token, {sameSite:"none", secure:true}).status(201).json({    //Name of token and Value of token
