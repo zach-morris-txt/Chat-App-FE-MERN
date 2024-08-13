feaf0bd342b3f331209a06288e426bbe87b1982f
Zach Morris <sbpayetc@gmail.com>
2024-08-13 03:39:52 -0400

    🔨 build(selectedContact logic & logo): Prepare for contact-specific inbox messages by handling contact selection in state

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { UserContext } from "./UserContext";
 
 
 export default function Chat() {
     const [ws, setWs] = useState(null);
     const [onlinePeople, setOnlinePeople] = useState({});
+    const [selectedContact, setSelectedContact] = useState(null);
+    const {username, id} = useContext(UserContext);
+
+
     useEffect(() => {
         const ws = new WebSocket("ws://localhost:4040");
         setWs(ws);
@@ -23,26 +29,33 @@ export default function Chat() {
             showOnlinePeople(messageData.online)
         }
     }
+    const onlinePeopleExcludeOurUser = {...onlinePeople};
+    delete onlinePeopleExcludeOurUser[id];
 
     return (
         <div className="flex h-screen">
             <div className="bg-blue-100 w-1/3 p-2">
-                <div className="text-blue-800 font-bold flex mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-                    </svg>
-                    MERN Chat
-                </div>
-                {Object.keys(onlinePeople).map(userId => (
-                    <div className="border-b border-gray-100 py-2 flex items-center gap-2 cursor-pointer">
-                        <Avatar username={onlinePeople[userId]} userId={userId} />
-                        <span className="text-gray-900">{onlinePeople[userId]}</span>
+                <Logo />
+                {Object.keys(onlinePeopleExcludeOurUser).map(userId => (
+                    <div key={userId} onClick={() => setSelectedContact(userId)} 
+                    className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer "+(userId === selectedContact ? "bg-blue-800" : "")}>
+                        {userId === selectedContact && (
+                            <div className="h-12 w-1 bg-blue-500 rounded-r-md items-center"></div>
+                        )}
+                        <div className="flex gap-2 py-2 pl-4">
+                            <Avatar username={onlinePeople[userId]} userId={userId} />
+                            <span className="text-gray-900">{onlinePeople[userId]}</span>
+                        </div>
                     </div>
                 ))} 
             </div>
             <div className="flex flex-col bg-blue-300 w-2/3 p-2">
                 <div className="flex-grow">
-                    messages with selected person
+                    {!selectedContact && (
+                        <div className="flex items-center justify-center h-full">
+                            <div className="text-gray-500">Select a contact</div>
+                        </div>
+                    )}
                 </div>
                 <div className="flex gap-1">
                     <input 
